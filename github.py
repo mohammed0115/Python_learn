3286e98e4f087023834376b37559a8910454c14d
mohammed <mohemmedkamal0115498898@gmail.com>
2020-10-06 16:13:41 +0200

    path to learn github

--- github.py
+++ github.py
@@ -8,7 +8,7 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/mohammed0115/Python_learn.git
 git push -u origin main
-
+https://stackoverflow.com/questions/20101994/git-pull-from-master-into-the-development-branch/20103414
 mohammed kamal
 balabalaavfddnfdjbbjkfgn
 """
