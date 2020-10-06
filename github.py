89d92b2160a7d1a18aaad42b36277fee6f962bc6
mohammed <mohemmedkamal0115498898@gmail.com>
2020-10-06 15:42:06 +0200

    learn how to push or pull or create new branch

--- github.py
+++ github.py
@@ -14,10 +14,53 @@ balabalaavfddnfdjbbjkfgn
 """
 
 
+
+
+
+
+
+
 """
 Any update or delete 
 
 git add .
 git commit -m ""
 git push origin branchname
+"""
+
+
+
+
+
+
+"""
+first pull called clone 
+git clone  https://github.com/mohammed0115/Python_learn.git
+
+"""
+
+
+
+
+
+
+"""
+recieve for github source
+
+git fetch
+git checkout main
+git pull origin main
+"""
+
+
+
+
+
+
+
+
+"""
+
+to create new branch 
+git checkout -b yourbranchname
 """
