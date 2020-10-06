4a6513983d59bed4b730861a66b8e394eebe1422
mohammed <mohemmedkamal0115498898@gmail.com>
2020-10-06 15:17:54 +0200

    add new commit

--- github.py
+++ github.py
@@ -1,4 +1,6 @@
 """
+at first Deployament app 
+
 echo "# Python_learn" >> README.md
 git init
 git add .
@@ -6,4 +8,16 @@ git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/mohammed0115/Python_learn.git
 git push -u origin main
+
+
+balabalaavfddnfdjbbjkfgn
+"""
+
+
+"""
+Any update or delete 
+
+git add .
+git commit -m ""
+git push origin branchname
 """
